816aea0052e67fa87da4ad6821987e2733e0591a
Ryan Clark <rclark010@gmail.com>
2023-10-19 11:02:41 -0600

    Get Tailwind rollin'

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			// Using modern `rgb`
+			// See: https://tailwindcss.com/docs/customizing-colors#using-css-variables
+			"ctd-green": 'rgb(var(--ctd-green) / <alpha-value>)',
+			"ctd-pink": 'rgb(var(--ctd-pink) / <alpha-value>)',
+			"ctd-yellow": 'rgb(var(--ctd-yellow) / <alpha-value>)',
+		},
 		extend: {},
 	},
 	plugins: [],
