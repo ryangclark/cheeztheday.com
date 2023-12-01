cb62ee713df3cd84a5bc0be2760013eb0fe1417f
Ryan Clark <rclark010@gmail.com>
2023-11-30 20:00:38 -0700

    Layout overhaul

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,57 @@
 <script>
 	import '../app.css';
+	import fbLogo from '$lib/assets/Facebook_Logo_Secondary.png';
+	import igLogo from '$lib/assets/Instagram_Glyph_White.png';
+	import emailIcon from '$lib/assets/mail_icon.svg?raw';
 </script>
 
-<slot />
+<div class="flex min-h-[100svh] flex-col justify-between">
+	<div class="lg:mx-auto lg:w-full lg:max-w-screen-lg">
+		<slot />
+	</div>
+
+	<footer class="mb-6 mt-8">
+		<div class="my-8 h-1" id="footer-hr"></div>
+		<div class="px-4 lg:mx-auto lg:max-w-screen-lg">
+			<div class="flex flex-col gap-3 font-museo text-sm">
+				<a
+					class="flex items-center gap-2"
+					href="mailto:cheezthedayfoodtruck@gmail.com"
+					><span class="fill-current text-white">
+						<!-- I don't know how to directly style the resulting `<svg>`,
+							so we style a `<span>` instead. -->
+						{@html emailIcon}
+					</span>cheezthedayfoodtruck@gmail.com</a
+				>
+
+				<a
+					class="flex items-center gap-2"
+					href="https://www.facebook.com/CheezTheDay/"
+					><img alt="Facebook logo" class="w-6" src={fbLogo} />Cheez The Day</a
+				>
+
+				<a
+					class="flex items-center gap-2"
+					href="https://www.instagram.com/cheezthedayfoodtruck"
+					><img
+						alt="Instagram logo"
+						class="w-6"
+						src={igLogo}
+					/>cheezthedayfoodtruck</a
+				>
+			</div>
+		</div>
+	</footer>
+</div>
+
+<style>
+	#footer-hr {
+		background: linear-gradient(
+			0.25turn,
+			rgb(var(--ctd-green)) 0%,
+			rgb(var(--ctd-yellow)) 62%,
+			rgb(var(--ctd-pink-light)) 86%,
+			rgb(var(--ctd-pink-dark)) 100%
+		);
+	}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,19 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		colors: {
-			// Using modern `rgb`
-			// See: https://tailwindcss.com/docs/customizing-colors#using-css-variables
-			"ctd-green": 'rgb(var(--ctd-green) / <alpha-value>)',
-			"ctd-pink": 'rgb(var(--ctd-pink) / <alpha-value>)',
-			"ctd-yellow": 'rgb(var(--ctd-yellow) / <alpha-value>)',
+		extend: {
+			colors: {
+				// Using modern `rgb`
+				// See: https://tailwindcss.com/docs/customizing-colors#using-css-variables
+				'ctd-green': 'rgb(var(--ctd-green) / <alpha-value>)',
+				'ctd-pink': 'rgb(var(--ctd-pink) / <alpha-value>)',
+				'ctd-yellow': 'rgb(var(--ctd-yellow) / <alpha-value>)',
+			},
+			fontFamily: {
+				handwritten: ['"Brisa W01 Regular"'],
+				museo: ['MuseoSlab-500', 'ui-serif'],
+			},
 		},
-		extend: {},
 	},
 	plugins: [],
 };
