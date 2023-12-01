fb500a9361189607d1f799cb5a4e6b0060a3255e
Ryan Clark <rclark010@gmail.com>
2023-11-30 20:00:04 -0700

    Home page overhaul

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,28 @@
 	import logo from '$lib/assets/CTD_logo.png';
 </script>
 
-<img alt="Cheez The Day logo" src={logo} />
-<h1 class="sr-only">Cheez The Day</h1>
-<div class="flex justify-center">
-	<p>Cincinnati's Grilled Cheez Truck</p>
+<div class="flex min-h-[100svh] flex-col justify-between">
+	<div class="flex justify-center">
+		<img
+			alt="Cheez The Day logo"
+			class="mt-6 md:w-4/5 lg:mt-20 lg:w-1/3"
+			src={logo}
+		/>
+	</div>
+
+	<h1 class="sr-only">Cheez The Day</h1>
+	<div class="flex flex-col items-center">
+		<p class="mx-4 my-8 text-center font-handwritten text-5xl md:text-5xl">
+			life is about experiences &ndash;<br class="lg:hidden" /> come have one!
+		</p>
+		<h2 class="my-4 font-museo text-lg tracking-wide lg:text-xl">
+			Cincinnati's grilled cheez truck
+		</h2>
+	</div>
+	<nav class="my-12 flex justify-center gap-6">
+		<a class="font-museo text-xl underline" href="/about">About</a>
+		<a class="font-museo text-xl underline" href="/contact">Contact</a>
+		<a class="font-museo text-xl underline" href="/events">Events</a>
+		<a class="font-museo text-xl underline" href="/menu">Menu</a>
+	</nav>
 </div>
