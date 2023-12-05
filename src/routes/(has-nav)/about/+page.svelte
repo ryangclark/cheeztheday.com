0faa8049b668098535d892f964f8de5f041148f9
Ryan Clark <rclark010@gmail.com>
2023-12-04 18:04:11 -0700

    Punching up the copy

--- src/routes/(has-nav)/about/+page.svelte
+++ src/routes/(has-nav)/about/+page.svelte
@@ -1,7 +1,14 @@
-<h1 class="my-3 text-3xl">About Us</h1>
+<h1 class="my-3 text-3xl">About us</h1>
 
 <!-- <h4 class="my-2 italic">Life is about experiences &ndash; Come have one!</h4> -->
 
+<p class="mb-2 mt-6">Cheez The Day is Cincinnati's grilled cheez truck.</p>
+<p class="mb-8">
+	It's where melty cheese meets toasted bread. Where good people find good food.
+	It's where you should be.
+</p>
+
+<h2 class="mb-3 mt-6 text-2xl">How it started</h2>
 <p class="my-2">
 	In 2020, Larry and Tony decided to test their lifelong friendship by going
 	into business together. With their wives, Connie and Terry, they opened a food
@@ -14,17 +21,19 @@
 	<a class="underline" href="/menu">our menu</a>.
 </p>
 
-<h2 class="mb-3 mt-6 text-2xl">Did You Know?</h2>
+<h2 class="mb-3 mt-6 text-2xl">Did you know?</h2>
 <ul class="list-disc">
-	<li class="ml-6">
+	<li class="mb-2 ml-6">
 		Tony is clasically-trained and experienced chef. He prefers slingin'
 		sandwiches.
 	</li>
-	<li class="ml-6">
-		Tony helped build out the truck himself. Elbow grease meets kitchen grease.
+	<li class="mb-2 ml-6">
+		Tony helped build out the truck himself, installing the griddle and welding
+		shit and everything. Elbow grease meets kitchen grease.
 	</li>
+	<li class="mb-2 ml-6 italic">more fun facts...</li>
 </ul>
 
 <h2 class="mb-3 mt-6 text-2xl">FAQ</h2>
-<p class="my-2 italic">Why aren't there more questions?</p>
+<p class="my-2 italic">Why aren't there any questions yet?</p>
 <p class="my-2">Huh. Good question.</p>
