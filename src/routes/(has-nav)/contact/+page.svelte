85c0b29570d6e389e01751e87c311b1b7ac95247
Ryan Clark <rclark010@gmail.com>
2023-12-12 07:59:20 -0700

    Further style tweaks, esp on desktop

--- src/routes/(has-nav)/contact/+page.svelte
+++ src/routes/(has-nav)/contact/+page.svelte
@@ -19,37 +19,39 @@
 	Not currently accepting recipe requests.
 </p> -->
 
-<h2 class="mb-2 mt-12 font-museo text-xl">Write us a note</h2>
+<div class="mx-auto flex max-w-xl flex-col">
+	<h2 class="mb-2 mt-12 font-museo text-xl">Write us a note</h2>
 
-<form class="mb-8 flex flex-col gap-2" method="POST">
-	<!-- <label class="mr-auto mt-4 font-mono" for="to"> To </label>
+	<form class="mb-8 flex flex-col gap-2" method="POST">
+		<!-- <label class="mr-auto mt-4 font-mono" for="to"> To </label>
 	<input class="lg:mr-auto" name="to" type="text" value="Cheez The Day" /> -->
 
-	<label class=" mt-4 font-mono" for="name"> Your name </label>
-	<input class="p-2" name="name" type="text" />
+		<label class=" mt-4 font-mono" for="name"> Your name </label>
+		<input class="p-2" name="name" type="text" />
 
-	<label class=" mt-4 font-mono" for="message"> Message </label>
-	<textarea class="min-h-[6rem] p-2" id="message" name="message" required
-	></textarea>
+		<label class=" mt-4 font-mono" for="message"> Message </label>
+		<textarea class="min-h-[6rem] p-2" id="message" name="message" required
+		></textarea>
 
-	<label class=" mt-4 font-mono" for="email"> Contact info </label>
-	<div class="flex gap-2 pr-2">
-		<input
-			autocomplete="email"
-			class="min-w-0 p-2"
-			name="email"
-			placeholder="Email"
-			type="email"
-		/>
-		<input
-			autocomplete="tel"
-			class="min-w-0 p-2"
-			name="phone"
-			placeholder="Phone"
-			type="text"
-		/>
-	</div>
+		<label class=" mt-4 font-mono" for="email"> Contact info </label>
+		<div class="flex justify-between gap-2">
+			<input
+				autocomplete="email"
+				class="min-w-0 p-2"
+				name="email"
+				placeholder="Email"
+				type="email"
+			/>
+			<input
+				autocomplete="tel"
+				class="min-w-0 p-2"
+				name="phone"
+				placeholder="Phone"
+				type="text"
+			/>
+		</div>
 
-	<!-- TODO: I'm not stoked on how this button is styled. Attempt improvements. -->
-	<button class="ctd-gradient-border mx-auto my-6 px-8 py-1">Send</button>
-</form>
+		<!-- TODO: I'm not stoked on how this button is styled. Attempt improvements. -->
+		<button class="ctd-gradient-border mx-auto my-6 px-8 py-1">Send</button>
+	</form>
+</div>
