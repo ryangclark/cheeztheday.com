b1a40b619915da935cbf8c216c0b23b7e520e813
Ryan Clark <rclark010@gmail.com>
2024-05-14 10:28:17 -0600

    Alternate env variable access

--- src/routes/(has-nav)/contact/+page.server.ts
+++ src/routes/(has-nav)/contact/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from './$types.js';
+import { PUBLIC_FORM_TO_EMAIL_URL } from '$env/static/public';
 
 /** @type {import('./$types').Actions} */
 export const actions = {
@@ -18,26 +18,23 @@ export const actions = {
 		const name = formData.get('name')?.toString().trim() || '';
 		const phone = formData.get('phone')?.toString().trim() || '';
 
-		if (!event.platform?.env.PUBLIC_FORM_TO_EMAIL_URL) {
+		if (!PUBLIC_FORM_TO_EMAIL_URL) {
 			console.log('error: no URL for Form-To-Email!');
 
 			return { success: false };
 		}
 
-		const emailResponse = await fetch(
-			event.platform?.env.PUBLIC_FORM_TO_EMAIL_URL,
-			{
-				body: JSON.stringify({
-					'cf-turnstile-response': cf_turnstile_response,
-					email,
-					message,
-					name,
-					phone,
-				}),
-				headers: { 'Content-Type': 'application/json' },
-				method: 'POST',
-			}
-		);
+		const emailResponse = await fetch(PUBLIC_FORM_TO_EMAIL_URL, {
+			body: JSON.stringify({
+				'cf-turnstile-response': cf_turnstile_response,
+				email,
+				message,
+				name,
+				phone,
+			}),
+			headers: { 'Content-Type': 'application/json' },
+			method: 'POST',
+		});
 
 		if (!emailResponse.ok) {
 			return { success: false };
