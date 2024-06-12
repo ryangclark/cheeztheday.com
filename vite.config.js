cf70518a16b58868bba821e1d9ea00d896f39d19
Ryan Clark <rclark010@gmail.com>
2024-06-12 16:16:06 -0600

    Updates dependencies; adds Svelte Inspector

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { svelteInspector } from '@sveltejs/vite-plugin-svelte-inspector';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), svelteInspector()],
 });
